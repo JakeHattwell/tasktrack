ac2aaf1d0c5b2c7512f1700208a1e62e5a7e5081
Jake Hattwell <j.hattwell@uq.edu.au>
2020-08-19 10:50:09 +1000

    feat: update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 window.onload = () => {
 	'use strict';
   
-	if ('serviceWorker' in navigator) {
-	  navigator.serviceWorker
-			   .register('./sw.js');
-	}
+	if ("serviceWorker" in navigator) {
+		if (navigator.serviceWorker.controller) {
+		  console.log(
+			"[PWA] Service Worker already found, skipping register"
+		  );
+		} else {
+		  // Register the service worker
+		  navigator.serviceWorker
+			.register("/service-worker.js", {
+			  scope: "./",
+			})
+			.then(function (reg) {
+			  console.log(
+				"[PWA] Service worker has been registered for scope: " +
+				  reg.scope
+			  );
+			});
+		}
+	  }
   }
